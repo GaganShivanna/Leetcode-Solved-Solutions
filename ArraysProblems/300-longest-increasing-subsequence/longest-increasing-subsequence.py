d368bd40f080946712e1740101b74f3f1c7a40c8
Gagan Shivanna <98219082+GaganShivanna@users.noreply.github.com>
2026-09-13 21:59:09 -0700

    Time: 2403 ms (7.57%) | Memory: 21.9 MB (9.39%) - LeetSync

--- ArraysProblems/300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ ArraysProblems/300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,10 +1,10 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        memo = [-1] * n
+        memo = {}
         def dfs(i):
 
-            if memo[i] != -1:
+            if i in memo:
                 return memo[i]
             LIS = 1
             for j in range(i + 1, n):
